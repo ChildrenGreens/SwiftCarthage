2e3f1034e02254a3ed5c7cd71482cc51631c1e92
caiqiujun <903400858@qq.com>
2016-12-19 02:46:44 +0800

    carthage test

---
+++ SwiftCarthage-iOS/SwiftCarthage-iOS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCarthage-iOS.h
+//  SwiftCarthage-iOS
+//
+//  Created by caiqiujun on 2016/12/19.
+//  Copyright © 2016年 caiqiujun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftCarthage-iOS.
+FOUNDATION_EXPORT double SwiftCarthage_iOSVersionNumber;
+
+//! Project version string for SwiftCarthage-iOS.
+FOUNDATION_EXPORT const unsigned char SwiftCarthage_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCarthage_iOS/PublicHeader.h>
+
+
---
+++ SwiftCarthage/SwiftCarthage.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCarthage.swift
+//  SwiftCarthage
+//
+//  Created by caiqiujun on 2016/12/19.
+//  Copyright © 2016年 caiqiujun. All rights reserved.
+//
+
+import UIKit
+
+class SwiftCarthage: NSObject {
+  
+  public class func showCarthage() {
+    print("hello, my carthage!")
+  }
+
+}
